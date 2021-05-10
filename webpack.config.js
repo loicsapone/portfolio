e687daa018c9514f326f555bb9e58f5605288586
Loïc Sapone <loic@sapone.fr>
2021-05-10 19:03:32 +0200

    Install Twig + Webpack Encore

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // path
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // entries
+    .addEntry('app', './assets/app.js')
+
+    // features
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSingleRuntimeChunk()
+    .enableSassLoader()
+
+    // babel config
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // dev-server config
+    .configureDevServerOptions(options => {
+        options.https = {
+            pfx: path.join(process.env.HOME, '.symfony/certs/default.p12'),
+        }
+    })
+;
+
+module.exports = Encore.getWebpackConfig();
