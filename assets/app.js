207bbba3f66afd4a75344e87a439885745bc1f21
Loïc Sapone <loic@sapone.fr>
2021-05-25 18:09:44 +0200

    Add animations
    anim
    
    animation

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,15 @@ import "@fontsource/arvo/700.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 import './styles/app.scss';
+
+// activate animations on sections
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('animate');
+        }
+    });
+});
+document.querySelectorAll('#about, #github, #contact').forEach(section => {
+    observer.observe(section);
+});
