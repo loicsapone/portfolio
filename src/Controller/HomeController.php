8eeb2fd59ef75ce875a1283138f3a09e92d24330
Loïc Sapone <loic@sapone.fr>
2021-06-01 17:57:23 +0200

    Move Github repository list in yaml file and load them in specific service

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Form\Type\ContactType;
+use App\Service\GithubService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,58 +17,11 @@ class HomeController extends AbstractController
         Route('/', name: 'home_view', methods: ['GET']),
         Cache(maxage: 604800, public: true)
     ]
-    public function __invoke(): Response
+    public function __invoke(GithubService $githubService): Response
     {
-        $repositories = [
-            [
-                'url' => 'https://github.com/loicsapone/portfolio',
-                'name' => 'loicsapone/portfolio',
-                'description' => 'My own website',
-                'createdAt' => new \DateTime('2021-05-10T16:07:03Z'),
-                'forked' => false,
-            ],
-            [
-                'url' => 'https://github.com/IQ2i/data-importer',
-                'name' => 'IQ2i/data-importer',
-                'description' => 'A PHP library to easily manage and import large data file',
-                'createdAt' => new \DateTime('2021-05-10T16:07:03Z'),
-                'forked' => false,
-            ],
-            [
-                'url' => 'https://github.com/IQ2i/aergie',
-                'name' => 'IQ2i/aergie',
-                'description' => 'An easy alternative to makefile',
-                'createdAt' => new \DateTime('2020-09-30T06:50:47Z'),
-                'forked' => false,
-            ],
-            [
-                'url' => 'https://github.com/IQ2i/prestashop-webservice-bundle',
-                'name' => 'IQ2i/prestashop-webservice-bundle',
-                'description' => 'Symfony bundle to wrap Prestashop official webservice client',
-                'createdAt' => new \DateTime('2015-04-10T19:32:37Z'),
-                'forked' => false,
-            ],
-            [
-                'url' => 'https://github.com/loicsapone/symfony-docs',
-                'name' => 'loicsapone/symfony-docs',
-                'description' => 'The Symfony documentation',
-                'createdAt' => new \DateTime('2010-02-17T08:43:51Z'),
-                'forked' => true,
-            ],
-            [
-                'url' => 'https://github.com/loicsapone/PrestaShop',
-                'name' => 'loicsapone/PrestaShop',
-                'description' => 'PrestaShop offers a fully scalable open source ecommerce solution.',
-                'createdAt' => new \DateTime('2012-11-19T16:41:31Z'),
-                'forked' => true,
-            ],
-        ];
-
-        $form = $this->createForm(ContactType::class);
-
         return $this->render('home/view.html.twig', [
-            'repositories' => $repositories,
-            'form' => $form->createView()
+            'repositories' => $githubService->getRepositories(),
+            'form' => $this->createForm(ContactType::class)->createView()
         ]);
     }
 }
---
+++ src/Service/GithubService.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\Yaml\Yaml;
+
+class GithubService
+{
+    public function __construct(
+        private string $filename
+    ) {}
+
+    public function getRepositories(): array
+    {
+        $data = Yaml::parseFile($this->filename, Yaml::PARSE_DATETIME);
+        return $data['github']['repositories'] ?? [];
+    }
+}
