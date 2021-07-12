24ca9abffcd283a0e0e484b9696c964eb8e873a8
Loïc Sapone <loic@sapone.fr>
2021-07-12 18:13:00 +0200

    Check spam before sending contact email (use Akismet)

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Form\Type\ContactType;
 use App\Query\ContactHandler;
 use App\Query\ContactQuery;
+use App\Service\AkismetService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,24 +15,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    public function __construct(
-        private ContactHandler $handler,
-    ) {}
-
     #[Route('/contact', name: 'contact_action', methods: ['POST'])]
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, ContactHandler $handler, AkismetService $akismetService): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $result = $this->handler->handle(new ContactQuery(
+            $contactQuery = new ContactQuery(
                 $form->get('name')->getData(),
                 $form->get('email')->getData(),
                 $form->get('message')->getData(),
-            ));
+            );
 
-            if ($result) {
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+                'comment_author' => $contactQuery->getName(),
+                'comment_author_email' => $contactQuery->getEmail(),
+                'comment_content' => $contactQuery->getMessage(),
+            ];
+
+            if ($akismetService->getSpamScore($context) < 2 && $handler->handle($contactQuery)) {
                 $this->addFlash('success', 'Votre demande de contact a bien été envoyée !');
                 return $this->redirectToRoute('home_view');
             }
---
+++ src/Service/AkismetService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class AkismetService
+{
+    public function __construct(
+        private HttpClientInterface $client,
+        private string $akismetApiKey,
+        private string $appDefaultUri,
+    ) {}
+
+    public function getSpamScore(array $context): int
+    {
+        $response = $this->client->request('POST', sprintf('https://%s.rest.akismet.com/1.1/comment-check', $this->akismetApiKey), [
+            'body' => array_merge($context, [
+                'blog' => $this->appDefaultUri,
+                'comment_type' => 'contact-form',
+                'comment_date_gmt' => (new \DateTime())->format('c'),
+                'blog_lang' => 'fr',
+                'blog_charset' => 'UTF-8',
+            ]),
+        ]);
+
+        $headers = $response->getHeaders();
+        if ('discard' === ($headers['x-akismet-pro-tip'][0] ?? '')) {
+            return 2;
+        }
+
+        $content = $response->getContent();
+        if (isset($headers['x-akismet-debug-help'][0])) {
+            throw new \RuntimeException(sprintf('Unable to check for spam: %s (%s).', $content, $headers['x-akismet-debug-help'][0]));
+        }
+
+        return 'true' === $content ? 1 : 0;
+    }
+}
