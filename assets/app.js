13666f7218a71e502a4154a93f3d5235d41de54b
Loïc Sapone <loic@sapone.fr>
2021-07-17 13:03:10 +0200

    Use AlpineJS instead of custom script

--- assets/app.js
+++ assets/app.js
@@ -3,22 +3,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 import './styles/app.scss';
 
-// activate animations on sections
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-            entry.target.classList.add('animate');
-        }
-    });
-});
-document.querySelectorAll('#about, #github, #contact').forEach(section => {
-    observer.observe(section);
-});
+import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
 
-// alert remove after 5 seconds
-const alert = document.querySelector('#flash-message');
-if (null !== alert) {
-    setTimeout(() => {
-        alert.parentNode.removeChild(alert);
-    },5000);
-}
+Alpine.plugin(intersect);
+Alpine.start()
